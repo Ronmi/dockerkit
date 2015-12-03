b602003e2fc5e2f2b1c021d6847c1345c2217e14
Ronmi Ren <ronmi.ren@gmail.com>
2015-12-04 01:11:09 +0800

    Add doc-comment

--- src/Distro/Distro.php
+++ src/Distro/Distro.php
@@ -4,7 +4,7 @@ namespace Fruit\DockerKit\Distro;
 
 use Fruit\DockerKit\Module;
 
-/// TODO: There might be some other system functionalities needed to be abstracted.
+/// TODO: There might be some other system functionalities need to be abstracted.
 interface Distro
 {
     /**
@@ -14,9 +14,32 @@ interface Distro
      */
     public function ensureBash();
 
+    /**
+     * Setting up system timezone.
+     */
     public function tz($tz);
+
+    /**
+     * Install packages.
+     * The structure of parameter array is up to implementation.
+     */
     public function install(array $pkgs);
+
+    /**
+     * Setting up package repositories.
+     * The structure of parameter array is up to implementation.
+     */
     public function repo(array $repos);
+
+    /**
+     * Setting up a package with package manage system provided tool.
+     * The structure of $config is up to implementation.
+     */
     public function pkgconf($pkg, $config = null);
+
+    /**
+     * Setting up package manage system.
+     * The structure of parameter array is up to implementation.
+     */
     public function pmsconf(array $config);
 }
